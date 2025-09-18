4807d16f649cb022461ab979eb0772a26407a99a
Wawan Sismadi <wawan@sismadi.com>
2025-09-18 12:05:05 +0700

    Update 02.js

--- 02.js
+++ 02.js
@@ -16,3 +16,23 @@ const mobil2 = new Mobil('Honda', 'Civic', 2021);    // Membuat objek mobil2
 // Memanggil metode untuk menampilkan informasi
 mobil1.displayInfo(); // Output: Mobil: Toyota Corolla (2020)
 mobil2.displayInfo(); // Output: Mobil: Honda Civic (2021)
+
+class Hewan {
+  speak() {
+    console.log("Hewan berbicara");
+  }
+}
+
+class Anjing extends Hewan {
+  speak() {
+    console.log("Anjing menggonggong");
+  }
+}
+
+let hewan1 = new Hewan();
+let hewan2 = new Anjing();
+
+hewan1.speak(); // Output: Hewan berbicara
+hewan2.speak(); // Output: Anjing menggonggong
+
+
